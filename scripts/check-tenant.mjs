b25243c7d2793e86345c90e4a22442ae3d3a9991
Matheus Coelho <matheuscoelhoo321@gmail.com>
2026-01-24 16:52:36 -0300

    fix

---
+++ scripts/check-tenant.mjs
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function check() {
+  await mongoose.connect(process.env.MONGODB_URI);
+
+  const tenant = await mongoose.connection.db.collection('tenants').findOne({ name: /movseven/i });
+
+  console.log('Tenant MovSeven:');
+  console.log(JSON.stringify(tenant, null, 2));
+
+  await mongoose.disconnect();
+}
+
+check();
